033a14d086812e0fd8aea380a21db7721a6d086d
george-chatzopoulos <georgechatzopoulos2805@gmai.com>
2025-01-07 17:33:03 +0200

    Remove dotenv

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,19 +1,7 @@
 package com.example.app;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class App {
     public static void main(String[] args) {
-        // Load environment variables
-        Dotenv dotenv = Dotenv.load();
-        String key = dotenv.get("API_KEY");
-
-        if (key == null) {
-            System.out.println("One or more environment variables were not found");
-            return;
-        }
-        System.out.println(key);
-
         // Hashmap
         HashMap<String, String> map = new HashMap<>();
         map.put("test", "test");
